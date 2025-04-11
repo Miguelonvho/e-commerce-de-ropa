39a19623cb568fa8d3b53c5eac0c5cbe55fa44da
Ivan-Gauto <ivangauto602160@gmail.com>
2025-04-11 16:57:52 -0300

    Estilizacion de cards

--- app/Views/front/plantilla.php
+++ app/Views/front/plantilla.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= base_url('public/assets/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('public/assets/css/miestilo.css') ?>">
-    <title>titulo</title>
+    <title>G&G Indumentaria</title>
 </head>
 
 <body>
@@ -38,10 +38,20 @@
                             <a class="text-white nav-link" href="#">Nosotros</a>
                         </li>
                     </ul>
-                    <form class="d-flex" role="search">
-                        <input class="bg-white form-control me-2" type="search" aria-label="Search">
-                        <button class="btn btn-outline-light" type="submit">Buscar</button>
-                    </form>
+                    <div class="d-flex gap-2 justify-content-between" style="height: 40px;">
+                        <ul class="d-flex flex-row navbar-nav">
+                            <li class="nav-item">
+                                <a class="btn" href=""><img src="<?= base_url('public/assets/img/carrito-nav.png') ?>" alt=""></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="btn" href=""><img style="width: 90%;" src="<?= base_url('public/assets/img/usuario.png') ?>" alt=""></a>
+                            </li>
+                        </ul>
+                        <form class="d-flex" role="search">
+                            <input class="bg-white form-control me-2" type="search" aria-label="Search">
+                            <button class="btn btn-outline-light" type="submit">Buscar</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </nav>
@@ -85,19 +95,23 @@
     <section class="d-flex justify-content-center p-4 align-items-center gap-5 container-fluid bg-black text-center text-white">
         <div class="iconos-informativos d-flex flex-column align-items-center">
             <img src="<?= base_url('public/assets/img/entrega.png') ?>" alt="">
-            <p class="pt-3 m-0">Entregas a todo el pais</p>
+            <p class="pt-3 m-0 fw-semibold">Entregas a todo el pais</p>
+            <a class="text-white" href="">Ver mas</a>
         </div>
         <div class="iconos-informativos d-flex flex-column align-items-center">
             <img src="<?= base_url('public/assets/img/metodo-de-pago.png') ?>" alt="">
-            <p class="pt-3 m-0">Todos los medios de pago</p>
+            <p class="pt-3 m-0 fw-semibold">Todos los medios de pago</p>
+            <a class="text-white" href="">Ver mas</a>
         </div>
         <div class="iconos-informativos d-flex flex-column align-items-center">
             <img src="<?= base_url('public/assets/img/cambio-de-ropa.png') ?>" alt="">
-            <p class="pt-3 m-0">Aceptamos cambios</p>
+            <p class="pt-3 m-0 fw-semibold">Aceptamos cambios</p>
+            <a class="text-white" href="">Ver mas</a>
         </div>
         <div class="iconos-informativos d-flex flex-column align-items-center">
             <img src="<?= base_url('public/assets/img/insignia.png') ?>" alt="">
-            <p class="pt-3 m-0">Mas de 1000 clientes satisfechos</p>
+            <p class="pt-3 m-0 fw-semibold">Mas de 1000 clientes satisfechos</p>
+            <a class="text-white" href="">Ver mas</a>
         </div>
     </section>
     <!-- Indumentaria destacada -->
@@ -105,39 +119,67 @@
         <div>
             <h1 class="text-black text-center pt-5">Destacado</h1>
         </div>
-        <div class="d-flex flex-wrap p-5 gap-4 text-white text-center justify-content-center">
-        <div class="bg-black text-white  card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="bg-black text-white card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-outline-light">Go somewhere</a>
+        <div class="d-flex flex-wrap p-5 gap-3 text-white justify-content-center">
+            <div class="bg-black text-white  card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="d-flex flex-column bg-black text-white card-body justify-content-between">
+                    <div class="d-flex flex-column gap-5">
+                        <h4>Nike Run-swift</h4>
+                    </div>
+                    <div class="d-flex flex-column gap-3 text-center">
+                        <h1 class="card-title">$xxxxxx</h1>
+                        <div class="d-flex gap-2 row-2 justify-content-end">
+                        <a href="#" class="btn btn-success">Comprar</a>
+                        <a href="#" class="btn btn-carrito btn-outline-secondary"><img src="<?= base_url('public/assets/img/carrito.png') ?>" alt=""></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="bg-black text-white card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="bg-black text-white card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-outline-light">Go somewhere</a>
+            <div class="bg-black text-white card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="d-flex bg-black text-white card-body flex-column justify-content-between">
+                    <div class="d-flex flex-column gap-5">
+                        <h4>Remera Essentials Stacked</h>
+                    </div>
+                    <div class="d-flex flex-column gap-3 text-center">
+                        <h1 class="card-title">$xxxxxx</h1>
+                        <div class="d-flex gap-2 row-2 justify-content-end">
+                        <a href="#" class="btn btn-success">Comprar</a>
+                        <a href="#" class="btn btn-carrito btn-outline-secondary"><img src="<?= base_url('public/assets/img/carrito.png') ?>" alt=""></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="bg-black text-white card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="bg-black text-white card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-outline-light">Go somewhere</a>
+            <div class="bg-black text-white card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="d-flex bg-black text-white card-body flex-column justify-content-between">
+                    <div class="d-flex flex-column gap-5">
+                        <h4>Jean Levi's</h4>
+                    </div>
+                    <div class="d-flex flex-column gap-3 text-center">
+                        <h1 class="card-title">$xxxxxx</h1>
+                        <div class="d-flex gap-2 row-2 justify-content-end">
+                        <a href="#" class="btn btn-success">Comprar</a>
+                        <a href="#" class="btn btn-carrito btn-outline-secondary"><img src="<?= base_url('public/assets/img/carrito.png') ?>" alt=""></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="bg-black text-white card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-outline-light">Go somewhere</a>
+            <div class="bg-black text-white card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="d-flex bg-black text-white card-body flex-column justify-content-between">
+                    <div class="d-flex flex-column gap-5">
+                        <h3>Buzo Nike</h3>
+                    </div>
+                    <div class="d-flex flex-column gap-3 text-center">
+                        <h1 class="card-title">$xxxxxx</h1>
+                        <div class="d-flex gap-2 row-2 justify-content-end">
+                        <a href="#" class="btn btn-success">Comprar</a>
+                        <a href="#" class="btn btn-carrito btn-outline-secondary"><img src="<?= base_url('public/assets/img/carrito.png') ?>" alt=""></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
         </div>
     </section>
     <!-- Footer -->
