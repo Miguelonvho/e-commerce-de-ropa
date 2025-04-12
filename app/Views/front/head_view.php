39d1ee51e091d091f66e8bd4f4c7af555b91e48d
Ivan-Gauto <ivangauto602160@gmail.com>
2025-04-12 14:37:17 -0300

    Actualizacion de codigo

---
+++ app/Views/front/head_view.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('public/assets/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('public/assets/css/miestilo.css') ?>">
+    <title><?= $titulo ?></title>
+</head>
