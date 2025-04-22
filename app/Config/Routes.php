3bae66f44f5a2ba1a1db76da383f09ee99b69445
Miguelonvho <garavagliamiguel23@gmail.com>
2025-04-21 21:57:05 -0300

    Se ruteo la pagina de contacto

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,4 +10,4 @@ $routes->get('plantilla_principal', 'Home::index');
 $routes->get('quienes_somos', 'Home::quienes_somos');
 $routes->get('plantilla_generos/(:segment)', 'Home::plantilla_generos/$1');
 $routes->get('comercializacion', 'Home::comercializacion');
-
+$routes->get('contacto', 'Home::contacto');
