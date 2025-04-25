ec1a811c57769434cf320dd11bbb91b9c9390f66
Ivan-Gauto <ivangauto602160@gmail.com>
2025-04-25 17:01:05 -0300

    Estilizacion de plantilla de perfil

--- app/Views/front/plantilla_perfil.php
+++ app/Views/front/plantilla_perfil.php
@@ -2,14 +2,19 @@
 <html lang="es">
 
 <body>
-    <h1>Mi informacion</h1>
-    <hr>
-    <div class="bg-black">
-        <img style="width: 90px; height: 90px; object-fit: cover; border-radius: 100%;" src="public/assets/img/persona-1.jpg" alt="">
-        <p>Nombre completo: Ivan Guillermo Gauto</p>
-        <p>Correo: ivangauto@gmail.com</p>
-        <p>telefono: 3777604408</p>
-        <p>pais: Argentina</p>
+    <h1 class="m-4 fw-light">Mi informacion</h1>
+    <div class="container rounded shadow-lg bg-black text-white p-5">
+        <img style="width: 200px; height: 200px; object-fit: cover; border-radius: 100%; margin-bottom: 20px;" src="public/assets/img/persona-1.jpg" alt="">
+        <hr>
+        <p>Nombre completo: <p class="fst-italic">Ivan Guillermo Gauto</p></p>
+        <hr>
+        <p>Correo: <p class="fst-italic">ivangauto@gmail.com</p></p>
+        <hr>
+        <p>telefono: <p class="fst-italic">3777604408</p></p>
+        <hr>
+        <p>pais: <p class="fst-italic">Argentina</p></p>
+        <hr>
+        <a style="width: auto;" href="" class="btn mt-4 boton-blanco">Cerrar sesión</a>
     </div>
 </body>
 
