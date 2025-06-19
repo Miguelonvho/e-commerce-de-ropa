a91371d228f198c2693bde571dadc9bece58b9f4
Ivan-Gauto <ivangauto602160@gmail.com>
2025-06-19 00:44:45 -0300

    Se cambio tiempo de muestra de las notificaciones

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toastElList = [].slice.call(document.querySelectorAll('.toast'));
     toastElList.forEach(function (toastEl) {
-        const toast = new bootstrap.Toast(toastEl);
+        const toast = new bootstrap.Toast(toastEl, { delay: 1500 });
         toast.show();
     });
 });
