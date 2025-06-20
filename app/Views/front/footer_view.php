58bb74f8ff5a5e4f37d34b5074b08a5f3bba3f06
Ivan <ivangauto602160@gmail.com>
2025-06-19 22:21:57 -0300

    Link de botones a paginas

--- app/Views/front/footer_view.php
+++ app/Views/front/footer_view.php
@@ -32,13 +32,22 @@
         <a class="text-white" href="<?= base_url('terminos_y_usos') ?>">Terminos y Usos</a>
     </div>
     <div class="d-flex flex-wrap justify-content-center gap-3 p-0">
-        <button class="btn btn-transparent"><img
-                src="<?= base_url(relativePath: 'public/assets/img/Iconos/instagram.png') ?>" alt=""></button>
-        <button class="btn btn-transparent"><img
-                src="<?= base_url(relativePath: 'public/assets/img/Iconos/whatsapp.png') ?>" alt=""></button>
-        <button class="btn btn-transparent"><img
-                src="<?= base_url(relativePath: 'public/assets/img/Iconos/facebook.png') ?>" alt=""></button>
+        <!-- Botón a Instagram -->
+        <a href="https://www.instagram.com/tu_usuario" target="_blank" class="btn btn-transparent">
+            <img src="<?= base_url('public/assets/img/Iconos/instagram.png') ?>" alt="Instagram">
+        </a>
+
+        <!-- Botón a WhatsApp (con número predeterminado) -->
+        <a href="https://wa.me/5491234567890" target="_blank" class="btn btn-transparent">
+            <img src="<?= base_url('public/assets/img/Iconos/whatsapp.png') ?>" alt="WhatsApp">
+        </a>
+
+        <!-- Botón a Facebook -->
+        <a href="https://www.facebook.com/tu_pagina" target="_blank" class="btn btn-transparent">
+            <img src="<?= base_url('public/assets/img/Iconos/facebook.png') ?>" alt="Facebook">
+        </a>
     </div>
+
     <div class="text-end text-white pe-5 mt-3">
         &copy; <?= date('Y') ?> G&G Indumentaria. Todos los derechos reservados.
     </div>
